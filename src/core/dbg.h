fe5cc5d7f749c948f7b57cad169a9696b6f04b49
Virgilio Sanz <virgilio.sanz@gmail.com>
2014-11-12 01:40:10 +0100

    Remove PCRE dependency - go to posix regex

--- src/core/dbg.h
+++ src/core/dbg.h
@@ -13,11 +13,11 @@
 
 #define clean_errno() (errno == 0 ? "None" : strerror(errno))
 
-#define log_err(M, ...) fprintf(stderr, "[ERROR] (%s:%d - %s: errno: %s) " M "\n", __FILE__, __LINE__, __func__, clean_errno(), ##__VA_ARGS__)
+#define log_err(M, ...) fprintf(stderr, "[ERROR] %s:%d - %s: errno: %s - " M "\n", __FILE__, __LINE__, __func__, clean_errno(), ##__VA_ARGS__)
 
-#define log_warn(M, ...) fprintf(stderr, "[WARN] (%s:%d - %s: errno: %s) " M "\n", __FILE__, __LINE__, __func__, clean_errno(), ##__VA_ARGS__)
+#define log_warn(M, ...) fprintf(stderr, "[WARN] %s:%d - %s: errno: %s - " M "\n", __FILE__, __LINE__, __func__, clean_errno(), ##__VA_ARGS__)
 
-#define log_info(M, ...) fprintf(stderr, "[INFO] (%s:%d - %s) " M "\n", __FILE__, __LINE__, __func__, ##__VA_ARGS__)
+#define log_info(M, ...) fprintf(stderr, "[INFO] %s:%d - %s - " M "\n", __FILE__, __LINE__, __func__, ##__VA_ARGS__)
 
 #define check(A, M, ...) if(!(A)) { log_err(M, ##__VA_ARGS__); errno=0; goto error; }
 
